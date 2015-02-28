0288f37d59fa481903a3537873a2eb7e1e925358
Palmer <palmercluff@gmail.com>
2015-02-27 17:07:11 -0700

    (Palmer) I added the EmailTest class, but I haven't gotten it to work yet.

---
+++ app/src/main/java/com/example/admin/roofexpertsapp/EmailTest.java
@@ -0,0 +1,14 @@
+package com.example.admin.roofexpertsapp;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Palmer on 2/27/2015.
+ */
+public class EmailTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+}
