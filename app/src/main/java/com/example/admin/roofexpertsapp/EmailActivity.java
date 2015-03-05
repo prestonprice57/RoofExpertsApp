1a4059309a9a3845864f874465ba139847569c30
Palmer <palmercluff@gmail.com>
2015-03-05 12:12:28 -0700

    Email works! But only on a real device. Also fixed the alignment in the personal info.

--- app/src/main/java/com/example/admin/roofexpertsapp/EmailActivity.java
+++ app/src/main/java/com/example/admin/roofexpertsapp/EmailActivity.java
@@ -1,64 +1,47 @@
 package com.example.admin.roofexpertsapp;
 
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+// (Palmer) From: http://examples.javacodegeeks.com/android/core/email/android-how-to-send-email/
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-
-public class EmailActivity extends ActionBarActivity {
-
+public class EmailActivity extends Activity {
+    Button button;
+    EditText destinationAddress;
+    EditText sbj;
+    EditText messageBody;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
-
-        // Making the button do stuff...
-        final Button button = (Button) findViewById(R.id.sendButton); // button_id
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                System.out.println("You pushed the button!"); // Output in Android Studio Log
-
-                // Get string from text view
-                EditText receiverString = (EditText) findViewById(R.id.receiver);
-                String str = receiverString.getText().toString();
-
-                Context context = getApplicationContext();
-                CharSequence text = "Your message to " + str + " has been sent";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+        sbj = (EditText) findViewById(R.id.subject);
+        messageBody = (EditText) findViewById(R.id.messageBody);
+        button = (Button) findViewById(R.id.button);
+        destinationAddress = (EditText) findViewById(R.id.destinationAddress);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String subject = sbj.getText().toString();
+                String message = messageBody.getText().toString();
+                String to = destinationAddress.getText().toString();
+                Intent emailActivity = new Intent(Intent.ACTION_SEND);
+                //set up the recipient address
+                emailActivity.putExtra(Intent.EXTRA_EMAIL, new String[] { to });
+                //set up the email subject
+                emailActivity.putExtra(Intent.EXTRA_SUBJECT, subject);
+                //you can specify cc addresses as well
+                // email.putExtra(Intent.EXTRA_CC, new String[]{ ...});
+                // email.putExtra(Intent.EXTRA_BCC, new String[]{ ... });
+                //set up the message body
+                emailActivity.putExtra(Intent.EXTRA_TEXT, message);
+                emailActivity.setType("message/rfc822");
+                startActivity(Intent.createChooser(emailActivity, "Select your Email Provider :"));
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_email, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
