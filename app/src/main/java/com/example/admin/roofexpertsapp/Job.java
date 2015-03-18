bbd340f860c5139f1b423134c134afac16605af6
TakeshiYanagita <LastSamurai297@gmail.com>
2015-03-17 23:49:42 -0600

    Added the TileEstimate class, TwoYearTuneUpJob class, and Job interface

---
+++ app/src/main/java/com/example/admin/roofexpertsapp/Job.java
@@ -0,0 +1,8 @@
+package com.example.admin.roofexpertsapp;
+
+/**
+ * Created by Takeshi on 3/17/2015.
+ */
+public interface Job {
+    void fillOut();
+}
---
+++ app/src/main/java/com/example/admin/roofexpertsapp/TwoYearTuneUpJob.java
@@ -0,0 +1,23 @@
+package com.example.admin.roofexpertsapp;
+
+/**
+ * Created by Takeshi on 3/17/2015.
+ */
+public class TwoYearTuneUpJob implements Job {
+    private int numTileBroken;
+    private String rake;
+    private String ridge;
+    private String type;
+    private int numTileChipped;
+    private int numTileMisplaced;
+    private int numCrown;
+    private int numPipe;
+    private int numChim;
+    private int numSky;
+
+
+
+    public void fillOut() {
+
+    }
+}
